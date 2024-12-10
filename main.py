67c6e96ddb0c6005eec803a7609ce7bc228884a1
Mathieu <bon.mathieu@gmail.com>
2024-12-10 14:38:35 +0100

    Timezone conversion functional.

--- main.py
+++ main.py
@@ -3,9 +3,15 @@ import pandas as pd
 from config import Config
 import matplotlib
 
-conn = psycopg2.connect(Config.DATABASE_URI)
 
-df = pd.read_sql_query(
-    "SELECT timestamp, temperature, humidity, name FROM records INNER JOIN devices ON devices.id = records.device_id ",
-    conn,
-)
+def get_temp_df() -> pd.DataFrame:
+    conn = psycopg2.connect(Config.DATABASE_URI)
+
+    df = pd.read_sql_query(
+        "SELECT timestamp, name, temperature, humidity FROM records INNER JOIN devices ON devices.id = records.device_id ",
+        conn,
+    )
+    df = df[df["name"].isin(["Chambre A&M", "Chambre Lucas", "Extérieur", "Salon"])]
+    return df
+
+df = get_temp_df()
